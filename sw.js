815cc9db6854f115d5a2669dc18c33547c3c37ed
tisbuet <tisbuet@gmail.com>
2022-10-02 14:06:12 +0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ea0a1582a74fbbe49f1.js"
+    "url": "webpack-runtime-fda07778034f8cfcc12d.js"
   },
   {
     "url": "framework-cfc39a90b11063613693.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "64e46483794fc8ec0c43d4a527f626d7"
+    "revision": "b8c130253f04e482dbe954f306fee51f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b5bc8a925215f7166f76.js"
