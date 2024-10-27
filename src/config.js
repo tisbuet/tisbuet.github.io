6a3ec84c55b9492b26a3aff0395c17e892b01524
tisbuet <tisbuet@gmail.com>
2024-10-28 01:35:33 +0600

    scholar url added

--- src/config.js
+++ src/config.js
@@ -14,6 +14,10 @@ module.exports = {
   github: 'https://github.com/shuvohishab',
   socialMedia: [
     {
+      name: 'Google Scholar',
+      url: 'https://scholar.google.com/citations?hl=en&user=iRSDOrUAAAAJ',
+    },
+    {
       name: 'GitHub',
       url: 'https://github.com/shuvohishab',
     },
